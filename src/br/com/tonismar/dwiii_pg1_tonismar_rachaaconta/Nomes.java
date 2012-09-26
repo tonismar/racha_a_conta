80c94b68f833982dc6a73d501e2d6efdb07aef9d
Tonismar <tonismar@gmail.com>
2012-09-25 22:57:44 -0300

    Complemento...

---
+++ src/br/com/tonismar/dwiii_pg1_tonismar_rachaaconta/Nomes.java
@@ -0,0 +1,12 @@
+package br.com.tonismar.dwiii_pg1_tonismar_rachaaconta;
+
+import java.io.Serializable;
+
+public class Nomes implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	String nomes;
+}
